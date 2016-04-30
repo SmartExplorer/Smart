6f487bde6fe1c6dfe909afb9eb80146dc49f2b69
HANPENG WANG <Kiljaeden@HANPENGs-MacBook-Air.local>
2016-04-30 23:10:57 +0800

    file structure

---
+++ src/testMain.cpp
@@ -0,0 +1,21 @@
+/*
+ * testMain.cpp
+ *
+ *  Created on: 30 Apr 2016
+ *      Author: Kiljaeden
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	double b = 1.111;
+	string a = "aaaaaaa";
+	const char* c = "abcde";
+	cout << a;
+	return 0;
+}
+
+
+
